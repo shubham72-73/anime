383f32d1ec0d8002a9893dcdcb6ec64e7addeef9
Shubham Singh <133738315+shubham72-73@users.noreply.github.com>
2024-10-14 10:14:20 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,41 @@
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
+
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
+  x[slideIndex-1].style.display = "block";
+  setTimeout(carousel, 5000); // Change image every 5 seconds
+}
+
+function showSidebar(){
+  const sidebar = document.querySelector(".sideBar");
+  sidebar.style.display = 'block';
+}
+
+function hideSidebar(){
+  const sideBar = document.querySelector(".sideBar");
+  sideBar.style.display = 'none';
+}
